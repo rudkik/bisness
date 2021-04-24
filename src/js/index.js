96b830f3033c3d212df991b328db60a744325136
rudkik <66820365+rudkik@users.noreply.github.com>
2021-04-24 16:08:09 +0300

    сверстал шапку начал сладйер

--- src/js/index.js
+++ src/js/index.js
@@ -19,6 +19,30 @@ Swiper.use([Navigation, Pagination, Autoplay, EffectFade, Thumbs]);
             });
         });
 
+
+        const swiper = new Swiper('.swiper-container', {
+            // Default parameters
+            slidesPerView: 1,
+            spaceBetween: 10,
+            // Responsive breakpoints
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 2,
+                    spaceBetween: 20
+                },
+                // when window width is >= 480px
+                480: {
+                    slidesPerView: 3,
+                    spaceBetween: 30
+                },
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 4,
+                    spaceBetween: 40
+                }
+            }
+        })
     });
 
 })(jQuery);
